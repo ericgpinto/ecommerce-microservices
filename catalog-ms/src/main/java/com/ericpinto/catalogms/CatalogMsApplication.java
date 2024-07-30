f098db21c6ccb4fcbca6acd94ce4e6b1dd8bbbb5
ericgpinto <ericgrandopinto@gmail.com>
2024-07-30 20:30:42 -0300

    (catalog-ms): initial commit

---
+++ catalog-ms/src/main/java/com/ericpinto/catalogms/CatalogMsApplication.java
@@ -0,0 +1,13 @@
+package com.ericpinto.catalogms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogMsApplication.class, args);
+    }
+
+}
---
+++ catalog-ms/src/test/java/com/ericpinto/catalogms/CatalogMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericpinto.catalogms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
